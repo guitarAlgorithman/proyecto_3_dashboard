90eee7b53607e27a4e23cd1dc7aad204199ff5b2
BootNM <nimeneses.dwfsc2@bootcampudd.cl>
2022-10-19 20:56:44 -0300

    Creo nueva rama y agrego a los js funcioonalidad de nombre de empresa en gráficos

--- finanzas.js
+++ finanzas.js
@@ -1,113 +1,138 @@
 (() => {
   const boton = document.getElementById("boton")
   let periodo = document.getElementById("periodo")
-  periodo.addEventListener("change",()=>{
-    if(periodo.value=="Intraday"){
-      frecuencia.disabled=false
+  periodo.addEventListener("change", () => {
+    if (periodo.value == "Intraday") {
+      frecuencia.disabled = false
     }
-    else{
-      frecuencia.disabled=true
+    else {
+      frecuencia.disabled = true
     }
   })
 
+  boton.addEventListener("click", async () => {
+// Consigo  el nombre de la Empresa desde API 
+
+    let stock1 = document.getElementById("accion").value;
+    let token = '&apikey=V3M13AGJZDLJ7SM0';
+    let urlBase1 = "https://www.alphavantage.co/query?function=";
+    let info = "OVERVIEW"
+    let url1 = [`${urlBase1}${info}&symbol=${stock1}${token}`]
+
+    const nombreEmpresa = fetch(url1)
+      .then((response) => response.json())
+      .then((item) => {
+        return item.Name;
+      });
+
+    const printName = async () => {
+      const nombre = await nombreEmpresa;
+      console.log(nombre);
+    };
+
+    printName();
+
+    
 
 
-  
-  boton.addEventListener("click", () => {
-      document.getElementById("graficoAcciones").innerHTML=""
-      let stock = document.getElementById("accion").value;        
-      let frecuencia = document.getElementById("frecuencia").value;        
-      let periodo = document.getElementById("periodo").value;
+    document.getElementById("graficoAcciones").innerHTML = ""
+    let prop = await nombreEmpresa;
+    // Guardo el nombre de la empresa para usarlo en lína .118
 
-      function getData(url) {
-          return Promise.resolve(
-              fetch(url)
-                  .then(res => res.json())
-          )
+    let stock = document.getElementById("accion").value;
+    let frecuencia = document.getElementById("frecuencia").value;
+    let periodo = document.getElementById("periodo").value;
+
+    function getData(url) {
+      return Promise.resolve(
+        fetch(url)
+          .then(res => res.json())
+      )
+    }
+    getUrl = (stock, per, freq) => {
+      //¿Como se esconde?
+      let token = '&apikey=V3M13AGJZDLJ7SM0';
+      let urlBase = "https://www.alphavantage.co/query?function=";
+      if (per == "Diario") {
+        let periodo = "TIME_SERIES_DAILY"
+        return [`${urlBase}${periodo}&symbol=${stock}${token}`, "Time Series (Daily)"]
+
       }
-      getUrl=(stock,per,freq)=>{
-        //¿Como se esconde?
-        let token='&apikey=V3M13AGJZDLJ7SM0';
-        let urlBase="https://www.alphavantage.co/query?function=";
-        if(per=="Diario"){
-          let periodo="TIME_SERIES_DAILY"
-          return [`${urlBase}${periodo}&symbol=${stock}${token}`,"Time Series (Daily)"]
-  
-        }
-        else if(per=="Mensual"){
-          let periodo="TIME_SERIES_MONTHLY"
-          return [`${urlBase}${periodo}&symbol=${stock}${token}`,"Monthly Time Series"]
-        }
-        else{
-          let periodo="TIME_SERIES_INTRADAY"
-          if(freq=="15 Minutos"){
-            frecuencia="&interval=15min"
-            return [`${urlBase}${periodo}&symbol=${stock}${frecuencia}${token}`,"Time Series (15min)"]
-          }
-          else if(freq=="30 Minutos"){
-            let frecuencia="&interval=30min"
-            //Time Series (30min)
-  
-            return [`${urlBase}${periodo}&symbol=${stock}${frecuencia}${token}`,"Time Series (30min)"]
-          }
-          else{
-            let frecuencia="&interval=60min"
-            return [`${urlBase}${periodo}&symbol=${stock}${frecuencia}${token}`,"Time Series (60min)"]
-          }
-        }
-  
-  
+      else if (per == "Mensual") {
+        let periodo = "TIME_SERIES_MONTHLY"
+        return [`${urlBase}${periodo}&symbol=${stock}${token}`, "Monthly Time Series"]
       }
-      //let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stock}&interval=30min&apikey=V3M13AGJZDLJ7SM0`;
-      let url=getUrl(stock,periodo,frecuencia)
-      //console.log(url[0])
-      getData(url[0]).then(data => {
-        //console.log(data);
+      else {
+        let periodo = "TIME_SERIES_INTRADAY"
+        if (freq == "15 Minutos") {
+          frecuencia = "&interval=15min"
+          return [`${urlBase}${periodo}&symbol=${stock}${frecuencia}${token}`, "Time Series (15min)"]
+        }
+        else if (freq == "30 Minutos") {
+          let frecuencia = "&interval=30min"
+          //Time Series (30min)
 
-          if (!data[url[1]]) {
-            swal('ERROR', 'Ingrese un texto válido', 'error')
-          } else {
-              let fechas = []
-              let valores = []
-              for (x in data[url[1]]) {
-                fechas.push(x.split(" ")[0])
-                valores.push({ 'x':x.split(" "),
-                 'y': [parseFloat(data[url[1]][x]['1. open']),
-                  parseFloat(data[url[1]][x]['2. high']),
-                   parseFloat(data[url[1]][x]['3. low']),
-                    parseFloat(data[url[1]][x]['4. close'])]
-                  })
-              }
-              fechas=fechas.reverse(x=>x)
-              valores=valores.reverse(x=>x)
-              
-              var options = {
-                  series: [{
-                  data: valores
-                }],
-                  chart: {
-                  type: 'candlestick',
-                  height: 350
-                },
-                title: {
-                  text: `Gráfico acción ${stock}`,
-                  align: 'center'
-                },
-                xaxis: {
-                  type: 'datetime'
-                },
-                yaxis: {
-                  tooltip: {
-                    enabled: true
-                  }
-                }
-                };
-                let chart = new ApexCharts(document.getElementById("graficoAcciones"), options);
-                chart.render();
-                
+          return [`${urlBase}${periodo}&symbol=${stock}${frecuencia}${token}`, "Time Series (30min)"]
+        }
+        else {
+          let frecuencia = "&interval=60min"
+          return [`${urlBase}${periodo}&symbol=${stock}${frecuencia}${token}`, "Time Series (60min)"]
+        }
+      }
+
+
+    }
+    //let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stock}&interval=30min&apikey=V3M13AGJZDLJ7SM0`;
+    let url = getUrl(stock, periodo, frecuencia)
+    //console.log(url[0])
+    getData(url[0]).then(data => {
+      //console.log(data);
+
+      if (!data[url[1]]) {
+        swal('ERROR', 'Ingrese un texto válido', 'error')
+      } else {
+        let fechas = []
+        let valores = []
+        for (x in data[url[1]]) {
+          fechas.push(x.split(" ")[0])
+          valores.push({
+            'x': x.split(" "),
+            'y': [parseFloat(data[url[1]][x]['1. open']),
+            parseFloat(data[url[1]][x]['2. high']),
+            parseFloat(data[url[1]][x]['3. low']),
+            parseFloat(data[url[1]][x]['4. close'])]
+          })
+        }
+        fechas = fechas.reverse(x => x)
+        valores = valores.reverse(x => x)
+
+        var options = {
+          series: [{
+            data: valores
+          }],
+          chart: {
+            type: 'candlestick',
+            height: 350
+          },
+          title: {
+            text: `Gráfico acción ${prop}`,
+            align: 'center'
+          },
+          xaxis: {
+            type: 'datetime'
+          },
+          yaxis: {
+            tooltip: {
+              enabled: true
+            }
           }
+        };
+        let chart = new ApexCharts(document.getElementById("graficoAcciones"), options);
+        chart.render();
 
-      })
+      }
+
+    })
   })
 }
 )()
--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 (() => {
 
-    const boton = document.getElementById("boton")
-    boton.addEventListener("click", async ()=>{
+    const boton = document.getElementById("boton");
 
-
-        document.getElementById("graficoAccionesProyectado").innerHTML=""
-        //El forecast sera con un periodo fijo
-        let stock = document.getElementById("accion").value;
+    boton.addEventListener("click", async () => {
+        // Consigo  el nombre de la Empresa desde API 
+        let stock1 = document.getElementById("accion").value;
         let token = '&apikey=V3M13AGJZDLJ7SM0';
+        let urlBase1 = "https://www.alphavantage.co/query?function=";
+        let info = "OVERVIEW"
+        let url1 = [`${urlBase1}${info}&symbol=${stock1}${token}`]
+
+        const nombreEmpresa = fetch(url1)
+        .then((response) => response.json())
+        .then((item) => {
+          return item.Name;
+        });
+        
+        const printName = async () => {
+            const nombre = await nombreEmpresa;
+            console.log(nombre);
+          };
+
+       printName();
+        
+        document.getElementById("graficoAccionesProyectado").innerHTML = ""
+        //El forecast sera con un periodo fijo
+
+        // Guardo el nombre de la empresa para usarlo en lína .99
+        let prop = await nombreEmpresa;
+        let stock = document.getElementById("accion").value;
         let urlBase = "https://www.alphavantage.co/query?function=";
         let periodo = "TIME_SERIES_DAILY"
         let url = [`${urlBase}${periodo}&symbol=${stock}${token}`, "Time Series (Daily)"]
-  
+
         function getData(url) {
             return Promise.resolve(
                 fetch(url)
@@ -19,84 +40,85 @@
             )
         }
         const data = getData(url[0])
-        data.then((data)=>{
-            let y=[]
-            let xx=[]
+        console.log(data);
+        data.then((data) => {
+            let y = []
+            let xx = []
             let anterior;
-            let k=true
-           
-            for(x in data[url[1]]){  
-                         
-                if(k==true){
-                    anterior=parseFloat(data[url[1]][x]['4. close'])
-                    k=false
-                }else{
+            let k = true
+
+            for (x in data[url[1]]) {
+
+                if (k == true) {
+                    anterior = parseFloat(data[url[1]][x]['4. close'])
+                    k = false
+                } else {
 
                     y.push(anterior)
                     xx.push(parseFloat(data[url[1]][x]['4. close']));
-                    anterior=parseFloat(data[url[1]][x]['4. close'])
+                    anterior = parseFloat(data[url[1]][x]['4. close'])
                 }
-                
+
             }
-            
-            xx=xx.reverse(x=>x)         
-            y=y.reverse(y=>y)
-            learnLinear= async (x,y)=>{
+
+            xx = xx.reverse(x => x)
+            y = y.reverse(y => y)
+            learnLinear = async (x, y) => {
 
                 const model = tf.sequential();
-                model.add(tf.layers.dense({units:1,inputShape:[1]}));
+                model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
                 model.compile({
-                    loss:'meanSquaredError',
-                     optimizer :'adamax'
-                    });
-                const xs=tf.tensor2d(x,[x.length,1]);
-                const ys=tf.tensor2d(y,[y.length,1]);
+                    loss: 'meanSquaredError',
+                    optimizer: 'adamax'
+                });
+                const xs = tf.tensor2d(x, [x.length, 1]);
+                const ys = tf.tensor2d(y, [y.length, 1]);
 
-                await model.fit(ys,xs,{epochs: 250});
-                let forecast=[]
-                let inicial=[y[(y.length)-1]];
-                
-                for(let i=0;i<10;i++){
-                    let tensor=model.predict(tf.tensor2d([inicial],[1,1]))
-                    let valor=parseFloat(tensor.dataSync()[0]).toFixed(2);
+                await model.fit(ys, xs, { epochs: 250 });
+                let forecast = []
+                let inicial = [y[(y.length) - 1]];
+
+                for (let i = 0; i < 10; i++) {
+                    let tensor = model.predict(tf.tensor2d([inicial], [1, 1]))
+                    let valor = parseFloat(tensor.dataSync()[0]).toFixed(2);
                     console.log(valor);
                     forecast.push(valor);
-                    inicial= tensor.dataSync()[0];
-                }      
-                
-                
+                    inicial = tensor.dataSync()[0];
+                }
+
+
                 var options = {
                     series: [{
-                    data: forecast
-                  }],
+                        data: forecast
+                    }],
                     chart: {
-                    type: 'line',
-                    height: 350
-                  },
-                  title: {
-                    text: `Gráfico Forecast 10 días ${stock}`,
-                    align: 'center'
-                  },                  
-                  yaxis: {
-                    tooltip: {
-                      enabled: true
+                        type: 'line',
+                        height: 350
+                    },
+                    title: {
+                        text: `Gráfico Forecast 10 días ${prop}`,
+                        align: 'center'
+                    },
+                    yaxis: {
+                        tooltip: {
+                            enabled: true
+                        }
                     }
-                  }
-                  };
-                  let chart2 = new ApexCharts(document.getElementById("graficoAccionesProyectado"), options);
-                  chart2.render();
-                
-                
-    
+                };
+                let chart2 = new ApexCharts(document.getElementById("graficoAccionesProyectado"), options);
+                chart2.render();
+
+
+
             }
-            learnLinear(xx,y)
+            learnLinear(xx, y)
 
 
 
 
-           
+
         })
-        
+
     })
 
 
