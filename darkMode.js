ca75f6bfa811811719844a12b1fc6054d29e719f
MarioASG <mariosanhuezag@hotmail.com>
2022-10-20 23:13:25 -0300

    Modo oscuro responsive

--- darkMode.js
+++ darkMode.js
@@ -1,8 +1,8 @@
 const bdark = document.querySelector('#bdark', );
 const body = document.querySelector('body');
 const inputs = document.querySelector('.inputs');
-const graficoAcciones = document.querySelector('.graficoAcciones')
-const graficoAccionesProyectado = document.querySelector('.graficoAccionesProyectado')
+const graficoAcciones = document.querySelector('.graficoAcciones');
+const graficoAccionesProyectado = document.querySelector('.graficoAccionesProyectado');
 const bdark2 = document.querySelector('.bwhite')
 
 bdark.addEventListener('click', e=>{
@@ -10,6 +10,5 @@ bdark.addEventListener('click', e=>{
     inputs.classList.toggle('inputsDark');
     graficoAcciones.classList.toggle('graficoAccionesDark');
     graficoAccionesProyectado.classList.toggle('graficoAccionesProyectadoDark');
-    bdark2.classList.toggle('bdark')
-})
-
+    bdark2.classList.toggle('bdark');
+});
