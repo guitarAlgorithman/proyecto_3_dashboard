dad556ec391e84f8a376421b03395d5e33fa07b3
sauriokat <gustavo.henriquez.m@gmail.com>
2022-10-20 22:05:38 -0300

    forecast modificado

--- finanzas.js
+++ finanzas.js
@@ -94,6 +94,7 @@
                   align: 'center'
                 },
                 xaxis: {
+                  
                   type: 'datetime'
                 },
                 yaxis: {
